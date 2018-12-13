18e429be2d6b651d9837cb9e6328f6bb3c1affef
Andrei Sakharuk <andreysakharuk@gmail.com>
2018-12-13 14:22:53 +0300

    commit

---
+++ src/test/java/Tests.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Tests {
+
+    @Test
+    public void testShouldBePassed() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testShouldBeFailed() {
+        Assert.assertTrue(true);
+    }
+
+}
